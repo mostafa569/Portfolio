1037fa78dc8480e1e6912ab5cfa16fb69595d7aa
Mostafa Mohamed <109727053+mostafa569@users.noreply.github.com>
2023-08-14 13:59:45 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+document.getElementById('downloadButton').addEventListener('click', function() {
+    var link = document.createElement('a');
+    link.href = '/CV1.pdf';
+    link.download = 'cv.pdf';
+    link.click();
+  });
